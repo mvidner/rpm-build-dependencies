2cac4031f33fa9976c7714d737e09685a516f756
Martin Vidner <mvidner@suse.cz>
2016-09-20 12:46:54 +0200

    Memoize cache_dir

--- print_deps.rb
+++ print_deps.rb
@@ -17,10 +17,12 @@ class Package
   end
 end
 
+$cache_dir = nil
 def cache_dir
+  return $cache_dir if $cache_dir
   name = File.expand_path("../cache", __FILE__)
   FileUtils.mkdir_p name unless File.directory? name
-  name
+  $cache_dir = name
 end
 
 # A persistent cache for an expensive operation that returns a string.
